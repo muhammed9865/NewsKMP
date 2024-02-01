7c043d0bd8f9791fab82cb7b45d6ce1014d51ab5
Muhammed Salman <mahmadslman@gmail.com>
2024-02-01 13:30:01 +0200

    fix: remote articles contain removed content

--- composeApp/src/commonMain/kotlin/com.salman.news/data/source/remote/impl/ArticlesRemoteDataSourceImpl.kt
+++ composeApp/src/commonMain/kotlin/com.salman.news/data/source/remote/impl/ArticlesRemoteDataSourceImpl.kt
@@ -1,23 +1,32 @@
 package com.salman.news.data.source.remote.impl
 
-import com.salman.news.data.source.remote.model.article.ArticlesDTO
 import com.salman.news.data.source.remote.ArticlesRemoteDataSource
+import com.salman.news.data.source.remote.model.article.ArticlesDTO
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
-import io.ktor.http.parameters
 
 /**
  * Created by Muhammed Salman email(mahmadslman@gmail.com) on 1/25/2024.
  */
 class ArticlesRemoteDataSourceImpl(
     private val client: HttpClient
-): ArticlesRemoteDataSource {
-    override suspend fun getTopHeadlines(page: Int, countryCode: String): ArticlesDTO {
-        return client.get("top-headlines") {
-            parameter("page", page)
-            parameter("country", countryCode)
-        }.body()
+) : ArticlesRemoteDataSource {
+    override suspend fun getTopHeadlines(page: Int, countryCode: String): Result<ArticlesDTO> {
+        return runCatching {
+            val response = client.get("top-headlines") {
+                parameter("page", page)
+                parameter("country", countryCode)
+            }.body<ArticlesDTO>().filterRemoved()
+
+
+            return Result.success(response)
+        }
+    }
+
+    private fun ArticlesDTO.filterRemoved(): ArticlesDTO {
+        val articles = articles.filterNot { it.isRemoved() }
+        return copy(articles = articles)
     }
 }
