1eec1ac702c139bc2a66d45942f2bf0bd3a54c54
Muhammed Salman <mahmadslman@gmail.com>
2024-02-07 18:16:47 +0200

    add: settings screen

--- composeApp/src/commonMain/kotlin/com.salman.news/presentation/App.kt
+++ composeApp/src/commonMain/kotlin/com.salman.news/presentation/App.kt
@@ -2,11 +2,14 @@ package com.salman.news.presentation
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import com.salman.news.presentation.navigation.BottomNavigationScreen
 import com.salman.news.presentation.theme.NewsTheme
 
+var LocalTopNavigator = compositionLocalOf<Navigator> { error("Not initialized") }
 @Composable
 fun App() {
     NewsTheme(
@@ -14,7 +17,9 @@ fun App() {
         dynamicColor = false
     ) {
         Navigator(BottomNavigationScreen()) {
-            SlideTransition(it)
+            CompositionLocalProvider(LocalTopNavigator provides it) {
+                SlideTransition(it)
+            }
         }
     }
 }
