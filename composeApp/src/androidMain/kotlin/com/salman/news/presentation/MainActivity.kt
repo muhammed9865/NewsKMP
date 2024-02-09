11b8a0b955663fe3a3b16a9a3edefd7de13d0dfc
Muhammed Salman <mahmadslman@gmail.com>
2024-02-10 00:52:57 +0200

    add: change app ui mode based on user preferences

--- composeApp/src/androidMain/kotlin/com/salman/news/presentation/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/salman/news/presentation/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val appEntryPoint: AppEntryContent by inject()
 
         setContent {
-            App()
+            appEntryPoint.Content()
         }
     }
 }
@@ -19,5 +21,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    AppEntryContent().Content()
 }
--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -2,8 +2,9 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.salman.news.data.di.PlatformDataModule
 import com.salman.news.di.sharedModule
-import com.salman.news.presentation.App
+import com.salman.news.presentation.AppEntryContent
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.java.KoinJavaComponent.inject
 
 /**
  * Created by Muhammed Salman email(mahmadslman@gmail.com) on 1/26/2024.
@@ -12,13 +13,13 @@ fun main() {
     startKoin {
         modules(sharedModule + PlatformDataModule().module)
     }
-
+    val appEntryPoint: AppEntryContent by inject(AppEntryContent::class.java)
     application {
         Window(
             title = "News",
             onCloseRequest = ::exitApplication,
         ) {
-            App()
+            appEntryPoint.Content()
         }
     }
 }
