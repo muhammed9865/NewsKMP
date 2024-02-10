2d56f7be4865fb30af53b53ccccebedeb2a640c3
Muhammed Salman <mahmadslman@gmail.com>
2024-02-10 03:02:34 +0200

    fix: ScreenModifier doesn't set background color

--- composeApp/src/commonMain/kotlin/com.salman.news/presentation/screen/ScreenUtil.kt
+++ composeApp/src/commonMain/kotlin/com.salman.news/presentation/screen/ScreenUtil.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import com.salman.news.presentation.theme.Dimens
@@ -14,8 +15,11 @@ import com.salman.news.presentation.theme.Dimens
  * Created by Muhammed Salman email(mahmadslman@gmail.com) on 1/25/2024.
  */
 val ScreenModifier
+    @Composable
+    @ReadOnlyComposable
     get() = Modifier
         .fillMaxSize()
+        .background(MaterialTheme.colorScheme.background)
         .padding(Dimens.ScreenPadding)
 
 @Composable
