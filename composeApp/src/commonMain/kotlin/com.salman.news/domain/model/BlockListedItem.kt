049bbfc46a3521127c6f1a9bd0c11ab422b226dc
Muhammed Salman <mahmadslman@gmail.com>
2024-02-14 22:51:59 +0200

    create: new models

---
+++ composeApp/src/commonMain/kotlin/com.salman.news/domain/model/BlockListedItem.kt
@@ -0,0 +1,10 @@
+package com.salman.news.domain.model
+
+/**
+ * Created by Muhammed Salman email(mahmadslman@gmail.com) on 2/10/2024.
+ */
+data class BlockListedItem(
+    val id: Long,
+    val source: String?,
+    val author: String?,
+)
